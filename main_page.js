d86e9ff8bd913ce03e7346e43783123365886d26
Chloe <cmlc3@cam.ac.uk>
2020-03-04 20:18:17 +0000

    Update on main page

---
+++ main_page.js
@@ -0,0 +1,8 @@
+document.addEventListener('mainpage', function() {
+    var button1 = document.getElementsByTagName('nav-item');
+    console.log(button1);
+
+    button1.addEventListener('click', function(event) {
+        alert("Page not developed yet")
+    })
+})
