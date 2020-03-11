20d41f6ee264e5f2c3659819d2d7702325557b6d
Chloe <cmlc3@cam.ac.uk>
2020-03-11 14:50:22 +0000

    Scroll

--- main_page.js
+++ main_page.js
@@ -5,4 +5,19 @@ document.addEventListener('DOMContentLoaded', function() {
     button1.addEventListener('click', function(event) {
         window.alert("Page not developed yet");
     })
+    
+    var image = document.getElementById('myImage');
+
+    var lastScrollTop = 0;
+
+    window.addEventListener("scroll", function(){
+    var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+
+    if (st > lastScrollTop){
+        image.style.opacity = '1';
+    } else {
+        image.style.opacity = '0'
+    }
+
+    });
 })
